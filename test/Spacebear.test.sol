77f3398404dd63c199e372738050d446ebaa73db
abinash.p <abinash.p@mindfiresolutions.com>
2024-01-29 13:05:39 +0530

    Files Added

--- test/Spacebear.test.sol
+++ test/Spacebear.test.sol
@@ -10,11 +10,27 @@ contract SpacebearTest is Test {
         spacebear = new Spacebear();
     }
     function testNameIsSpacebear() public {
-        assertEq(spacebear.name(),"Spacebears");   
+        assertEq(spacebear.name(),"Spacebear");   
     }
     function testMintingNTFs() public{
         spacebear.safeMint(msg.sender);
         assertEq(spacebear.ownerOf(0),msg.sender);
         assertEq(spacebear.tokenURI(0),"https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/spacebear_1.json");
+    }
+
+    function testMintWrongOwner() public{
+        address purchaser = address(0x1);
+        vm.startPrank(purchaser);
+        vm.expectRevert();
+        spacebear.safeMint(purchaser);
+        vm.stopPrank();
+    }
+    function testNFTBuyToken() public{
+        address purchaser = address(0x2);
+        vm.startPrank(purchaser);
+        spacebear.buyToken();
+        vm.stopPrank();
+        assertEq(spacebear.ownerOf(0),purchaser);
+
     }
 }
